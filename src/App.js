fb14a034ea9940f730e26da64f6845f622f40c30
SchemeTech <schemetechlimited@gmail.com>
2023-09-10 16:54:26 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import SimpleSlider from "./component/owl-carousel";
+import Logo from "../src/images/login/Icon.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section>
+        <div className="bg-[#EBF5FF] w-full h-[960px]">
+          <div className="grid grid-cols-6">
+            <div className="pt-[136px] ml-[118px]">
+              <div className="w-[575px] h-[636px] gap-[32px] login">
+                <div className="flex content-center">
+                  <img
+                    src={Logo}
+                    className="m-auto mt-[65px] mb-[32px]"
+                    alt="Logo"
+                  />
+                </div>
+
+                <p className="text-center mb-[32px] login_text">Login</p>
+
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="email_input mb-[16px]"
+                />
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="email_input mb-[16px]"
+                />
+
+                <div className="custom-checkbox">
+                  <input
+                    type="checkbox"
+                    id="remember_me"
+                    name="remember_me"
+                    value="remember_me"
+                  />
+                  <label htmlFor="remember_me" className="remember_me">
+                    Remember Me
+                  </label>
+                </div>
+
+                <div className="login_btn">
+                  <p className="login_btn_text ">Login</p>
+                </div>
+              </div>
+            </div>
+
+            
+            {/* <SimpleSlider /> */}
+            
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
---
+++ src/component/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <h1 className='text-8xl'>I love bd....</h1>
+    </div>
+  )
+}
